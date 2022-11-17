b0c6357311d968f8eb667df5e46aa6ece45dce8c
Ricardo Langbecker <rlangbecker94@gmail.com>
2022-11-17 16:09:23 -0300

    commit

---
+++ src/main/java/mongobdmapa/br/com/mongobdmapa/MongobdMapaApplication.java
@@ -0,0 +1,13 @@
+package mongobdmapa.br.com.mongobdmapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongobdMapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongobdMapaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mongobdmapa/br/com/mongobdmapa/MongobdMapaApplicationTests.java
@@ -0,0 +1,13 @@
+package mongobdmapa.br.com.mongobdmapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongobdMapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
